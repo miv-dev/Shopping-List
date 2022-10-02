0b9c15c17e954dbbe645ad74af9d700af8461acc
miv-dev <i@mike39.ru>
2022-10-02 15:27:30 +0300

    Setup activity_shop_item.xml

---
+++ app/src/main/java/com/miv_dev/shopping_list/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.miv_dev.shopping_list.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miv_dev.shopping_list.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
